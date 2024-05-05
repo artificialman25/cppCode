7eaa995477c6a2fa11dd6c039ed7a9f6521c2c91
Varun Saxena <109892712+varuns-2002@users.noreply.github.com>
2024-05-05 14:44:55 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubble_sort(int a[],int n)
+{   int temp;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+              temp=a[j];
+              a[j]=a[j+1];
+              a[j+1]=temp;
+            }
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int a[100];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    bubble_sort(a,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+ return 0;
+}
